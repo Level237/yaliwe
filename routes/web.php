4553a55d512343171dd40a1fe7aabbab39145935
imagic <aleximagic2020@gmail.com>
2022-11-15 09:05:26 +0100

    imagic

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('layouts.frontend.homepage');
 });
+Route::get('admin_dashbord', function () {
+    return view('admin_dashbord.index_admin');
+})->name('admin_dashbord');
