8e0362e31e25d8184cf956cf0187186394f5b01f
level237 <Bramslevel129@gmail.com>
2022-11-17 20:41:10 -0500

    test an store can be stored

--- tests/Feature/StoreTest.php
+++ tests/Feature/StoreTest.php
@@ -2,6 +2,7 @@
 
 namespace Tests\Feature;
 
+use App\Models\Store;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
@@ -13,10 +14,16 @@ class StoreTest extends TestCase
      *
      * @return void
      */
-    public function test_example()
-    {
-        $response = $this->get('/');
+    public function test_an_store_can_be_stored(){
 
-        $response->assertStatus(200);
+        $response=$this->post('stores',[
+            'name'=>'new Store',
+            'address_id'=>1,
+            'image_id'=>1,
+            'status'=>'pending'
+        ]);
+
+        //$response->assertRedirect(route('admin.stores.index'));
+        $this->assertCount(1,Store::all());
     }
 }
